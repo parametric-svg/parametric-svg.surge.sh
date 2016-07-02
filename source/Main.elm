edcaccd236b66e43ab1843a3462e98a5e347442f
Tomek <tomek@lystable.com>
2016-07-02 20:07:07 +0100

    Polymer yeah!

--- source/Main.elm
+++ source/Main.elm
@@ -1,9 +1,40 @@
-import Html exposing (h1, text, Html)
+import Html exposing (node, div, text, Html)
 import Html.App exposing (beginnerProgram)
+import Html.Attributes exposing (attribute)
 
 main : Program Never
 main = beginnerProgram
   { model = ()
-  , view = \_ -> h1 [] [text "Hello world!"]
+  , view = view
   , update = \_ -> \_ -> ()
   }
+
+
+-- MODEL
+
+type alias Model =
+  ()
+
+
+-- UPDATE
+
+type alias Message =
+  ()
+
+
+-- VIEW
+
+view : Model -> Html Message
+view _ =
+  node "paper-header-panel"
+    [ attribute "mode" "waterfall"
+    ]
+    [ node "paper-toolbar" []
+      [ div []
+        [ text "parametric-svg"
+        ]
+      ]
+    , div []
+      [ text "(content goes here)"
+      ]
+    ]

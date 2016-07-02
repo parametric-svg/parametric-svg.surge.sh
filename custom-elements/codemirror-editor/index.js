efd217368f5458b1ba857677665aad8fb709124b
Tomek <tomek@lystable.com>
2016-07-03 00:19:17 +0100

    CodeMirror styling

--- custom-elements/codemirror-editor/index.js
+++ custom-elements/codemirror-editor/index.js
@@ -1,20 +1,42 @@
 /* eslint-disable prefer-template */
 const codemirror = require('codemirror');
 const fs = require('fs');
+const jssLite = require('jss-lite');
 
-const codemirrorStyle = fs.readFileSync(
+require('codemirror/mode/xml/xml');
+
+const baseCss = fs.readFileSync(
   __dirname + '/../../node_modules/codemirror/lib/codemirror.css', 'utf8'
 );
+const themeCss = fs.readFileSync(
+  __dirname + '/../../node_modules/codemirror/theme/material.css', 'utf8'
+);
+const styleOverrides = jssLite({
+  '.CodeMirror': {
+    'font-family': '"source code pro", monospace',
+  },
+});
 
 const prototype = Object.assign(Object.create(HTMLElement.prototype), {
   createdCallback() {
     const shadow = this.createShadowRoot();
 
     const style = document.createElement('style');
-    style.textContent = codemirrorStyle;
+    style.textContent = baseCss + themeCss + styleOverrides;
     shadow.appendChild(style);
 
-    codemirror(shadow);
+    const editor = codemirror(shadow, {
+      mode: 'xml',
+      theme: 'material',
+      tabSize: 2,
+      inputStyle: 'contenteditable',
+      autofocus: true,
+    });
+    editor.setOption('extraKeys', {
+      Tab: (cm) => {
+        cm.replaceSelection('  ', 'end');
+      },
+    });
   },
 });
 

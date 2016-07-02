e951fdc758664df0e367f63a660f0c1558e7b38b
Tomek <tomek@lystable.com>
2016-07-02 21:02:01 +0100

    Prepare elm-css styles

--- source/components/ParametricSvgEditor.elm
+++ source/components/ParametricSvgEditor.elm
@@ -5,6 +5,17 @@ module ParametricSvgEditor exposing
 
 import Html exposing (node, div, text, Html)
 import Html.Attributes exposing (attribute)
+import Html.CssHelpers exposing (withNamespace)
+import Css.Namespace exposing (namespace)
+import Css exposing
+  ( Stylesheet
+  , stylesheet, (.)
+  , height
+  , pct
+  )
+
+{class} =
+  withNamespace componentNamespace
 
 
 -- MODEL
@@ -33,6 +44,7 @@ view : Model -> Html Message
 view _ =
   node "paper-header-panel"
     [ attribute "mode" "waterfall"
+    , class [Root]
     ]
     [ node "paper-toolbar" []
       [ div []
@@ -43,3 +55,19 @@ view _ =
       [ text "(content goes here)"
       ]
     ]
+
+
+-- STYLES
+
+type Classes
+  = Root
+
+css : Stylesheet
+css = (stylesheet << namespace componentNamespace)
+  [ (.) Root
+    [ height (pct 100)
+    ]
+  ]
+
+componentNamespace : String
+componentNamespace = "3de78af-"

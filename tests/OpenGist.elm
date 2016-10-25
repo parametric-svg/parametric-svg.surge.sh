168aad4bcc4f3150a8d32754a36709c2f6a0a87f
Tomek Wiszniewski <tomek@lystable.com>
2016-10-26 00:21:00 +0200

    Convert to fuzz test

--- tests/OpenGist.elm
+++ tests/OpenGist.elm
@@ -1,8 +1,10 @@
 module OpenGist exposing (all)
 
 import Expect
-import Test exposing (test, describe, Test)
+import Fuzz exposing (tuple3, string, map, Fuzzer)
+import Test exposing (describe, fuzz, Test)
 import Json.Decode exposing (decodeString)
+import Regex exposing (replace, regex, HowMany(All))
 
 import Components.OpenGist.Decoders exposing (userGists)
 
@@ -36,18 +38,20 @@ all =
       --           ]
       --         )
 
-      [ test "temp" <|
-        \() ->
+      [ fuzz threeJsonNeutralStrings
+        "works for multiple files in a single gist"
+        <|
+        \(id, basename1, basename2) ->
           """
           [
             {
-              "id": "327b60fd3db9d21d7155a93c9f5d154d",
+              "id": \""""++ id ++"""\",
               "files": {
-                "a.parametric.svg": {
-                  "filename": "a.parametric.svg"
+                \""""++ basename1 ++""".parametric.svg": {
+                  "filename": \""""++ basename1 ++""".parametric.svg"
                 },
-                "b.parametric.svg": {
-                  "filename": "b.parametric.svg"
+                \""""++ basename2 ++""".parametric.svg": {
+                  "filename": \""""++ basename2 ++""".parametric.svg"
                 }
               }
             }
@@ -55,7 +59,27 @@ all =
           """
             |> decodeString userGists
             |> Expect.equal
-              (Ok [["b", "a"]])
+              (Ok [[basename2, basename1]])
 
       ]
     ]
+
+
+threeJsonNeutralStrings : Fuzzer (String, String, String)
+threeJsonNeutralStrings =
+  tuple3
+    ( map ((++) "a") jsonNeutralString
+    , map ((++) "b") jsonNeutralString
+    , map ((++) "c") jsonNeutralString
+    )
+
+
+jsonNeutralString : Fuzzer String
+jsonNeutralString =
+  string
+    |> map
+      ( replace
+        All
+        (regex "[\\\\\"\n]")
+        (always "_")
+      )
